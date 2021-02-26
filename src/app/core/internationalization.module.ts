bd21daaaa14767aa55c848f777ffb83d1943b628
Danilo Hoffmann <d.hoffmann@intershop.de>
2021-02-26 17:11:22 +0100

    fix: prevent flickering translation for production mode

--- src/app/core/internationalization.module.ts
+++ src/app/core/internationalization.module.ts
@@ -3,8 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import localeDe from '@angular/common/locales/de';
 import localeFr from '@angular/common/locales/fr';
 import { Inject, LOCALE_ID, NgModule } from '@angular/core';
+import { TransferState } from '@angular/platform-browser';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { SSR_LOCALE } from './configurations/state-keys';
 
 export function translateFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
@@ -22,10 +25,19 @@ export function translateFactory(http: HttpClient) {
   ],
 })
 export class InternationalizationModule {
-  constructor(@Inject(LOCALE_ID) lang: string, translateService: TranslateService) {
+  constructor(
+    transferState: TransferState,
+    translateService: TranslateService,
+    @Inject(LOCALE_ID) angularDefaultLocale: string
+  ) {
     registerLocaleData(localeDe);
     registerLocaleData(localeFr);
 
-    translateService.setDefaultLang(lang.replace(/\-/, '_'));
+    let defaultLang = angularDefaultLocale.replace(/\-/, '_');
+    if (transferState.hasKey(SSR_LOCALE)) {
+      defaultLang = transferState.get(SSR_LOCALE, defaultLang);
+    }
+    translateService.setDefaultLang(defaultLang);
+    translateService.use(defaultLang);
   }
 }
